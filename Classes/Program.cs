586181c5329814240bcf4851581143bb6da7c8d0
Kenneth Baker <spyksaturn@gmail.com>
2023-02-13 15:41:11 -0500

    Initial Commit

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -1,10 +1,24 @@
-﻿namespace Classes
+﻿using Microsoft.VisualBasic;
+
+namespace Classes
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Car myCar = new Car();
+            myCar.Make = "Dodge";
+            myCar.Model = "Challenger";
+            myCar.Year = 2022;
+
+            
+            
+            Console.WriteLine($"Make: {myCar.Make}");
+            Console.WriteLine($"Model: {myCar.Model}");
+            Console.WriteLine($"Year: {myCar.Year}");
+            
+
+        
         }
     }
 }
